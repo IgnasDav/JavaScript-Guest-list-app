c2ae9d1ade7cf6803ce407a0d4b4d57954e95acb
IgnasDav <ignas321@gmail.com>
2021-09-28 21:02:53 +0300

    Found a bug inside a loop, trying to fix it

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let guestList = [];
+//Global variables
+
+const btnAddGuestEnd = document.querySelector(".btn__add__end");
+const btnAddGuestStart = document.querySelector(".btn__add__start");
+const btnRemoveGuestStart = document.querySelector(".btn__remove__start");
+const btnRemoveGuestEnd = document.querySelector(".btn__remove__end");
+const btnReverse = document.querySelector(".btn__reverse");
+const listDiv = document.createElement("div");
+const list = document.createElement("ol");
+const main = document.querySelector(".main");
+const input = document.querySelector("#name__input");
+const errorMessage = document.createElement("p");
+
+errorMessage.textContent = "Wrong Input";
+
+main.append(listDiv);
+
+btnAddGuestStart.addEventListener("click", (event) => {
+  event.preventDefault();
+  let inputValue = input.value;
+  for (let i = 0; i <= guestList.length; i++) {
+    if (inputValue === "" || !isNaN(inputValue)) {
+      main.append(errorMessage);
+    } else {
+      const listItem = document.createElement("li");
+      listDiv.append(list);
+      list.append(listItem);
+      listItem.append(inputValue);
+      errorMessage.remove();
+      //This line is a bug, the for loop keeps iterating over and over again
+      guestList.unshift(inputValue);
+    }
+  }
+});

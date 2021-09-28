c45e1668caf751bf7d3bdca8c26b68e4ae8b6dac
IgnasDav <ignas321@gmail.com>
2021-09-28 21:45:31 +0300

    Creating remove functions for the list

--- index.js
+++ index.js
@@ -13,25 +13,69 @@ const list = document.createElement("ol");
 const main = document.querySelector(".main");
 const input = document.querySelector("#name__input");
 const errorMessage = document.createElement("p");
+const listItem = document.createElement("li");
 
 errorMessage.textContent = "Wrong Input";
 
 main.append(listDiv);
-
+//Add guest list at the start
 btnAddGuestStart.addEventListener("click", (event) => {
   event.preventDefault();
   let inputValue = input.value;
-  for (let i = 0; i <= guestList.length; i++) {
-    if (inputValue === "" || !isNaN(inputValue)) {
-      main.append(errorMessage);
-    } else {
-      const listItem = document.createElement("li");
+  if (inputValue === "" || !isNaN(inputValue)) {
+    main.append(errorMessage);
+  } else {
+    const listItem = document.createElement("li");
+    guestList.unshift(inputValue);
+    errorMessage.remove();
+    listItem.append(inputValue);
+    for (let i = 0; i < guestList.length; i++) {
+      list.prepend(listItem);
       listDiv.append(list);
-      list.append(listItem);
-      listItem.append(inputValue);
-      errorMessage.remove();
-      //This line is a bug, the for loop keeps iterating over and over again
-      guestList.unshift(inputValue);
     }
   }
 });
+//Add guest list at the end
+btnAddGuestEnd.addEventListener("click", (event) => {
+  event.preventDefault();
+  let inputValue = input.value;
+  if (inputValue === "" || !isNaN(inputValue)) {
+    main.append(errorMessage);
+  } else {
+    const listItem = document.createElement("li");
+    guestList.push(inputValue);
+    errorMessage.remove();
+    listItem.append(inputValue);
+    for (let i = 0; i < guestList.length; i++) {
+      list.append(listItem);
+      listDiv.append(list);
+    }
+  }
+});
+//Remove guest from the start of the list
+btnRemoveGuestStart.addEventListener("click", (event) => {
+  event.preventDefault();
+  let inputValue = input.value;
+  guestList.shift(inputValue);
+  for (let i = guestList.length; i > 0; i--) {
+    const listItem = document.createElement("li");
+    listItem[i].textContent = null;
+  }
+});
+//Remove guest from the end of the list
+// btnRemoveGuestEnd.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   let inputValue = input.value;
+//   if (inputValue === "" || !isNaN(inputValue)) {
+//     main.append(errorMessage);
+//   } else {
+//     const listItem = document.createElement("li");
+//     guestList.push(inputValue);
+//     errorMessage.remove();
+//     listItem.append(inputValue);
+//     for (let i = 0; i < guestList.length; i++) {
+//       list.append(listItem);
+//       listDiv.append(list);
+//     }
+//   }
+// });

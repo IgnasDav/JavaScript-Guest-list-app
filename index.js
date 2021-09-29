7b63cfb2739d196b597421d18c14192a25864680
IgnasDav <ignas321@gmail.com>
2021-09-29 20:10:04 +0300

    Added bonus excercices to the app, css will be added later

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ const btnRemoveGuestStart = document.querySelector(".btn__remove__start");
 const btnRemoveGuestEnd = document.querySelector(".btn__remove__end");
 const btnReverse = document.querySelector(".btn__reverse");
 const listDiv = document.createElement("div");
-
+const btnAddGuestNumber = document.querySelector(".btn__add__guest");
+const btnRemoveGuestNumber = document.querySelector(".btn__remove__guest");
+const inputAddGuest = document.querySelector(".number__input--add");
+const inputRemoveGuest = document.querySelector(".number__input--remove");
 const main = document.querySelector(".main");
 const input = document.querySelector("#name__input");
 const errorMessage = document.createElement("p");
 const listItem = document.createElement("li");
-
+const btnMoveLastToFirst = document.querySelector(".btn__moveLastToFirst");
+const btnMoveFirstToLast = document.querySelector(".btn__moveFirstToLast");
 errorMessage.textContent = "Wrong Input";
 
-function drawlist() {
+function drawList() {
   const list = document.createElement("ol");
   listDiv.innerHTML = null;
   for (let i = 0; i < guestList.length; i++) {
@@ -38,7 +42,7 @@ btnAddGuestStart.addEventListener("click", (event) => {
   } else {
     errorMessage.remove();
     guestList.unshift(inputValue);
-    drawlist();
+    drawList();
   }
 });
 // Add guest list at the end
@@ -50,7 +54,7 @@ btnAddGuestEnd.addEventListener("click", (event) => {
   } else {
     errorMessage.remove();
     guestList.push(inputValue);
-    drawlist();
+    drawList();
   }
 });
 //Remove guest from the start of the list
@@ -58,17 +62,47 @@ btnAddGuestEnd.addEventListener("click", (event) => {
 btnRemoveGuestStart.addEventListener("click", (event) => {
   event.preventDefault();
   guestList.shift();
-  drawlist();
+  drawList();
 });
 //Remove guest from the end of the list
 btnRemoveGuestEnd.addEventListener("click", (event) => {
   event.preventDefault();
   guestList.pop();
-  drawlist();
+  drawList();
 });
 //Reversing list
 btnReverse.addEventListener("click", (event) => {
   event.preventDefault();
   guestList.reverse();
-  drawlist();
+  drawList();
+});
+//Removing certain guest from a list
+btnRemoveGuestNumber.addEventListener("click", (event) => {
+  event.preventDefault();
+  const removeValue = inputRemoveGuest.value;
+  guestList.splice(removeValue - 1, 1);
+  drawList();
+});
+//Adding certain guest to the list
+btnAddGuestNumber.addEventListener("click", (event) => {
+  event.preventDefault();
+  const addValue = inputAddGuest.value;
+  let inputValue = input.value;
+  guestList.splice(addValue - 1, 0, inputValue);
+  drawList();
+});
+//Moving Guest from last to first
+btnMoveLastToFirst.addEventListener("click", (event) => {
+  event.preventDefault();
+  const lastValue = guestList.pop();
+  guestList.unshift(lastValue);
+  drawList();
+});
+
+//Moving guest from first to last
+btnMoveFirstToLast.addEventListener("click", (event) => {
+  event.preventDefault();
+  const firstValue = guestList.shift();
+  guestList.push(firstValue);
+  drawList();
 });
